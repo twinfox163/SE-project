dca2363d6f7011b03f1c9817e895217e7adbc0f6
zzh <zhaozihao163126@163.com>
2024-06-08 20:13:28 +0800

    文本框改进

--- src/components/FileShow.vue
+++ src/components/FileShow.vue
@@ -12,7 +12,22 @@
 
 <template>
     <div>
-        当前显示文件：{{ file_path }}<br>
-        文件内容:<br/><input type="text" v-model="content">
+      <div>
+        {{ file_path }}<br>
+      </div>
+      <textarea v-model="content" class="file-content" @keydown.tab.prevent></textarea>
     </div>
-</template>
+  </template>
+  
+  <style>
+  .file-content {
+    width: 80%;
+    height: 300px; /* 设置输入框的高度 */
+    resize: vertical; /* 允许垂直方向调整大小 */
+    border: 1px solid #ccc; /* 添加边框 */
+    padding: 10px; /* 添加内边距 */
+    font-size: 14px; /* 设置字体大小 */
+    line-height: 1.6; /* 设置行高 */
+    font-family: Arial, sans-serif; /* 设置字体 */
+  }
+  </style>
