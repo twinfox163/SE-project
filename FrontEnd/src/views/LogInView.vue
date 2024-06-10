695b6fedb90f9d756d55075226b2adcf02d60731
zzh <zhaozihao163126@163.com>
2024-06-10 21:59:17 +0800

    登录部分代码

--- FrontEnd/src/views/LogInView.vue
+++ FrontEnd/src/views/LogInView.vue
@@ -1,49 +1,32 @@
-<script>
-    import axios from 'axios';
-    import Cookies from 'js-cookie';
-    export default{
-        data(){
-            return {
-                //id:"111",
-                password:"111",
-                username:"0123456789"
-            }
-        },
-        methods: {
-            login(){
-                const baseUrl = import.meta.env.VITE_API_BASE_URL + "/login";
-                console.log(baseUrl);
-                axios.get(baseUrl,{
-                    params:{
-                        username:this.username,
-                        password:this.password
-                    }
-                })
-                .then((response)=>{
-                    const {status,data}=response;
-                    if(data){
-                        //成功
-                        //alert("登录成功");
-                        Cookies.set('username',this.username);
-                        this.$router.push('/');
-                    }
-                    else{
-                        alert("登录失败");
-                    }
-                })
-            }
-        }
-    }
-
-
-</script>
-
 <template>
     <div>
-    用户名: <input type="text" v-model="username"><br>
-    <!-- 账号：<input type="text" v-model="id"><br> -->
-    密码: <input type="text" v-model="password"><br>
+    用户名:<input type="text" v-model="username"><br>
+    密码<input type="text" v-model="password"><br>
     <button @click="login()">log in</button>
     <router-link to="/signup">sign up</router-link>
     </div>
 </template>
+
+<script setup>
+    import axios from 'axios';
+    import Cookies from 'js-cookie';
+    import { ref } from 'vue';
+    import router from '@/router';
+    const username=ref("");
+    const password=ref("");
+    const login=()=>{
+        const url = import.meta.env.VITE_API_BASE_URL + "/login";
+        const params = {params:{username:username.value,password:password.value}};
+        console.log(url,params);
+        axios.get(url,params).then(response=>{
+            const {status,data}=response;
+            console.log(response);
+            if(data){
+                Cookies.set('username',username.value);
+                router.push('/');
+            }else{
+                alert('登录失败');
+            }
+        })
+    }
+</script>
