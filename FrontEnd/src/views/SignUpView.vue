ed43cd4b81c568e1b44d92594a1d5147c5c46f03
zzh <zhaozihao163126@163.com>
2024-06-10 21:32:52 +0800

    注册部分代码

--- FrontEnd/src/views/SignUpView.vue
+++ FrontEnd/src/views/SignUpView.vue
@@ -1,40 +1,24 @@
-<script>
-    import axios from 'axios';
-    export default{
-        data(){
-            return {
-                name:"hello",
-                //id:"111",
-                password:"111"
-            }
-        },
-        methods: {
-            signup(){
-                const baseUrl = import.meta.env.VITE_API_BASE_URL + "/signup";
-                console.log(baseUrl);
-                axios.get(baseUrl,{
-                    params :{
-                        //id:this.id,
-                        username: this.name,
-                        password: this.password
-                    }
-                })
-                .then(response=>{
-                    const {status,data} = response;
-                    alert(data);
-                })    
-            },
-        }
-    }
-
-
-</script>
-
 <template>
     <div>
-    用户名:<input type="text" v-model="name"><br>
-    <!-- 账号: <input type="text" v-model="id"><br> -->
-    密码: <input type="text" v-model="password"><br>
-    <button @click="signup()">注册</button>
+    用户名<input type="text" v-model="username"><br>
+    密码<input type="text" v-model="password"><br>
+    <button @click="signup()">sign up</button>
     </div>
 </template>
+
+<script setup>
+    import axios from 'axios'
+    import { ref } from 'vue';
+    const username = ref("");
+    const password = ref("");
+    const signup=()=>{
+        const url = import.meta.env.VITE_API_BASE_URL+"/signup";
+        const params = {params:{username:username.value,password:password.value}};
+        console.log(url,params);
+        axios.get(url,params).then(response=>{
+            const {status,data} = response;
+            console.log(response);
+            alert(data);
+        })
+    }
+</script>
