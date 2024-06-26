de3eb4b60ab93ee2a2813b77990977ba93fa4491
zzh <zhaozihao163126@163.com>
2024-06-26 21:05:07 +0800

    tiny fix

---
+++ FrontEnd/src/components/History.vue
@@ -0,0 +1,13 @@
+<script setup>
+    const props = defineProps({
+        history:Object
+    })
+</script>
+
+<template>
+    <div>
+        <div v-for="item in history" :key="item.time">
+            {{item.log}} by {{ item.username }} at {{ item.time }}
+        </div>
+    </div>
+</template>
